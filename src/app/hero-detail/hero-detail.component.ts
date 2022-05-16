d2a0c8b9fe406a6428ba4c254c381bb248a7ee38
rkfranco <93990385+rkfranco@users.noreply.github.com>
2022-05-16 10:38:21 -0300

    Add files via upload

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -11,14 +12,14 @@ import { HeroService } from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
-
+  hero: Hero | undefined;
+  
   constructor(
     private route: ActivatedRoute,
     // O ActivatedRoutecontém informações sobre a 
     // rota para esta instância do HeroDetailComponent.
     private heroService: HeroService,
-    // O HeroServiceobtém dados de herói do 
+    // O HeroService obtém dados de herói do 
     // servidor remoto e este componente os 
     // usará para obter o herói para exibição.
     private location: Location
@@ -36,6 +37,12 @@ export class HeroDetailComponent implements OnInit {
     // O paramMapé um dicionário de valores de parâmetros de rota extraídos da URL. A "id"chave retorna o iddo herói para buscar.
     //Os parâmetros de rota são sempre strings. A função JavaScript Numberconverte a string em um número, que é o que um herói iddeve ser.
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    if(this.hero){
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack()); 
+      }
   }
 
   goBack(): void {
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,18 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
+add(name: string): void {
+  name = name.trim();
+  if (!name) {return;}
+  this.heroService.addHero({name} as Hero).subscribe(hero => {this.heroes.push(hero);
+  });
+}
+
+delete(hero: Hero): void {
+  this.heroes = this.heroes.filter(h => h != hero);
+  this.heroService.deleteHero(hero.id).subscribe();
+}
+
 getHeroes(): void {
   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   // this.heroes = this.heroService.getHeroes();
